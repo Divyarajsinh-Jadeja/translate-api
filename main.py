c99dd6b6c3f96ea213b0e4fb792e685e142da46a
Divyarajsinh <divyarajsinh.jadeja@bytestechnolab.com>
2025-06-07 18:14:21 +0530

    Fix: bind to correct port for Render

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ import torch
 
 app = FastAPI()
 
+@app.get("/")
+def read_root():
+    return {"message": "Hello from Translate API!"}
+
 model_name = "facebook/m2m100_418M"
 tokenizer = M2M100Tokenizer.from_pretrained(model_name)
 model = M2M100ForConditionalGeneration.from_pretrained(model_name)
